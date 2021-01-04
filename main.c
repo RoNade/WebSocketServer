9bd68f4144571200c080c8e80b827266f7c36f5e
rnade <rnade@witekio.com>
2021-01-04 17:33:57 +0000

    switched server model to pub/sub message broker through websockets

--- main.c
+++ main.c
@@ -4,9 +4,45 @@
 #include <unistd.h>
 #include <libwebsockets.h>
 
+// created for each message
+
+struct msg {
+    void* payload;
+    size_t len;
+};
+
+// created for each client connecting to us
+
+struct per_session_data {
+    struct per_session_data* pss_list; // next link
+    struct lws* wsi;
+    uint32_t tail;
+    char publishing;
+};
+
+// created for each vhost out protocol is used with
+
+struct per_vhost_data {
+    struct lws_context* context;
+    struct lws_vhost* vhost;
+    const struct lws_protocols *protocols;
+    struct per_session_data* pss_list; // head link
+    struct lws_ring* ring;
+};
+
+// destory a message when all sucribers received a copy
+
+static void destroy_message(void* _msg)
+{
+    struct msg* msg = _msg;
+    free(msg->payload);
+    msg->payload = NULL;
+    msg->len = 0;
+}
+
 // function called by the libwebsocket service every time a request occurs
 
-static int callback_http
+static int callback_broker
 (
     struct lws* wsi,
     enum lws_callback_reasons reason,
@@ -15,96 +51,191 @@ static int callback_http
     size_t len
 )
 {
+    struct per_session_data* pss = user;
+    struct lws_vhost* vhost = lws_get_vhost(wsi);
+    const struct lws_protocols* protocols = lws_get_protocol(wsi);
+    struct per_vhost_data* vhd = lws_protocol_vh_priv_get(vhost, protocols);
+
+    const struct msg* pmsg;
+    struct msg amsg;
+    char buf[32];
+    int n, m;
+
     switch (reason)
-    {    
-        case LWS_CALLBACK_ESTABLISHED:
+    {
+        case LWS_CALLBACK_PROTOCOL_INIT:
         {
-            printf("Connection established\n");
+            vhd = lws_protocol_vh_priv_zalloc(vhost, protocols, sizeof(struct per_vhost_data));
+
+            vhd->context = lws_get_context(wsi);
+            vhd->protocols = protocols;
+            vhd->vhost = vhost;
+
+            // create a circular buffer for message queuing
+
+            vhd->ring = lws_ring_create(sizeof(struct msg), 8, destroy_message);
+
+            if (!vhd->ring)
+            {
+                return 1;
+            }
+
             break;
         }
-        case LWS_CALLBACK_HTTP:
+        case LWS_CALLBACK_PROTOCOL_DESTROY:
         {
-            char* requested_uri = (char*) in;
-            printf("Requested URI: %s\n", requested_uri);
+            lws_ring_destroy(vhd->ring);
 
-            if (strcmp(requested_uri, "/") == 0)
+            break;
+        }
+        case  LWS_CALLBACK_ESTABLISHED:
+        {
+            pss->tail = lws_ring_get_oldest_tail(vhd->ring);
+            pss->wsi = wsi;
+
+            // find out if client is a publisher or a subscriber
+
+            if (lws_hdr_copy(wsi, buf, sizeof(buf), WSI_TOKEN_GET_URI) > 0)
             {
-                void* universal_response = "Hello, World!";
-                lws_write(wsi, universal_response, strlen(universal_response), LWS_WRITE_HTTP);
-
-                break;
-            } 
-            else
-            {
-                // try to get current working directory
-
-                char cwd[1024];
-                char* resource_path;
-
-                if (getcwd(cwd, sizeof(cwd)) != NULL)
-                {
-                    // allocate enough memory for the resource path
-
-                    resource_path = malloc(strlen(cwd) + strlen(requested_uri));
-
-                    // join current working directory to the resource path
-
-                    sprintf(resource_path, "%s%s", cwd, requested_uri);
-                    printf("Resource path: %s\n", resource_path);
-
-                    char* extension = strrchr(resource_path, '.');
-                    char * mime;
-
-                    // choose the mime type based on the file extension
-
-                    if (extension == NULL)
-                    {
-                        mime = "text/plain";
-                    }
-                    else if (strcmp(extension, ".png") == 0)
-                    {
-                        mime = "image/png";
-                    }
-                    else if (strcmp(extension, ".jpg") == 0)
-                    {
-                        mime = "image/jpg";
-                    }
-                    else if (strcmp(extension, ".gif") == 0)
-                    {
-                        mime = "image/gif";
-                    }
-                    else if (strcmp(extension, ".html") == 0)
-                    {
-                        mime = "text/html";
-                    }
-                    else if (strcmp(extension, "css") == 0)
-                    {
-                        mime = "text/css";
-                    }
-                    else
-                    {
-                        mime = "text/plain";
-                    }
-                    
-                    // non-exisiting resources return 404
-
-                    lws_serve_http_file(wsi, resource_path, mime, NULL, 0);
-
-                    free(resource_path);
-                }
+                pss->publishing = !strcmp(buf, "/publisher");
             }
+
+            // client is not a publisher
+
+            if (!pss->publishing)
+            {
+                // add subscribers to the list of live pss
+
+                lws_ll_fwd_insert(pss, pss_list, vhd->pss_list);
+            }
+
+            break;
+        }
+        case LWS_CALLBACK_CLOSED:
+        {
+            // remove the pss from the list of live pss when a session closes
+
+            lws_ll_fwd_remove(struct per_session_data, pss_list, pss, vhd->pss_list);
+
+            break;
+        }
+        case LWS_CALLBACK_SERVER_WRITEABLE:
+        {
+            // sending pending message to subscribers if any
+
+            if (pss->publishing)
+            {
+                break;
+            }
+
+            // retrieve pending message
+
+            pmsg = lws_ring_get_element(vhd->ring, &pss->tail);
+
+            if (!pmsg)
+            {
+                break;
+            }
+
+            // LWS_PRE bytes are already allocated in the payload for protocol info
+
+            m = lws_write(wsi, pmsg->payload + LWS_PRE, pmsg->len, LWS_WRITE_TEXT);
+
+            if (m < pmsg->len)
+            {
+                lwsl_err("ERROR %d writing to ws socket\n", m);
+                return -1;
+            }
+
+            // consume message once sent and move ring tail
+
+            lws_ring_consume_and_update_oldest_tail
+            (
+                vhd->ring, // lws_ring object
+                struct per_session_data, // type of objects keeping track of tail
+                &pss->tail, // tail of currently active session
+                1, // number of payload being consumed
+                vhd->pss_list, // head of list of tail tracking objects
+                tail, // member name of tail in tracking objects
+                pss_list // member name of next object in tracking objects
+            );
+
+            // check if there is any message still pending
+
+            if (lws_ring_get_element(vhd->ring, &pss->tail))
+            {
+                // come back when the connection socket is able
+                // to accept another write packet without blocking
+
+                // if it already was able to take another packet without blocking
+                // you'll get this callback at the next call to the service loop
+
+                lws_callback_on_writable(pss->wsi);
+            }
+
+            break;
         }
         case LWS_CALLBACK_RECEIVE:
         {
-            char msg[len];
-            memcpy(msg, in, len);
-            msg[len] = '\0';
+            // adding messages from publishers to lws_ring object
 
-            printf("Data received: %s\n", msg);
-            
-            char response[] = "OK\0";
-            lws_write(wsi, response, strlen(response), LWS_WRITE_TEXT);
-            
+            if (!pss->publishing)
+            {
+                break;
+            }
+
+            // we ignore publishing when no subscribers are connected
+
+            if (!vhd->pss_list)
+            {
+                break;
+            }
+
+            // check how much free space is left in the ring buffer
+
+            n = lws_ring_get_count_free_elements(vhd->ring);
+
+            if (!n)
+            {
+                lwsl_user("Dropping message, no more free space!!\n");
+                break;
+            }
+
+            amsg.len = len;
+
+            // we over-allocate with LWS_PRE for protocol info insertion
+
+            amsg.payload = malloc(LWS_PRE + len);
+
+            if (!amsg.payload)
+            {
+                lwsl_user("Dropping message, out of memory!!\n");
+                break;
+            }
+
+            memcpy(amsg.payload + LWS_PRE, in, len);
+
+            // add message to lws_ring object
+
+            if (!lws_ring_insert(vhd->ring, &amsg, 1))
+            {
+                destroy_message(&amsg);
+                lwsl_user("Dropping message, failed insertion!!");
+                break;
+            }
+
+            // let every subscriber know we want to write
+            // something to them as soon as they are ready
+
+            lws_start_foreach_llp(struct per_session_data **, ppss, vhd->pss_list)
+            {
+                if (!(*ppss)->publishing)
+                {
+                    lws_callback_on_writable((*ppss)->wsi);
+                }
+            }
+            lws_end_foreach_llp(ppss, pss_list);
+
             break;
         }
         default:
@@ -118,14 +249,33 @@ static int callback_http
     return 0;
 }
 
+ // this sets a per-vhost, per-protocol option name:value pair
+ // the effect is to set this protocol to be the default one for the vhost
+ 
+static const struct lws_protocol_vhost_options pvo_opt = {
+        NULL,
+        NULL,
+        "default",
+        "1"
+};
+ 
+static const struct lws_protocol_vhost_options pvo = {
+        NULL,
+        &pvo_opt,
+        "lws-broker",
+        ""
+};
+
 // list of supported protocols and callbacks
 
 static struct lws_protocols protocols[] = {
     {
-        // first protocol must always be HTTP handler
-
-        "http-only",
-        callback_http,
+        "lws-broker",
+        callback_broker,
+        sizeof(struct per_session_data),
+        128,
+        0,
+        NULL,
         0
     },
     {
@@ -133,6 +283,7 @@ static struct lws_protocols protocols[] = {
 
         NULL,
         NULL,
+        0,
         0
     }
 };
@@ -144,6 +295,7 @@ int main(void)
     struct lws_context_creation_info info = {
         .port = 9000,
         .protocols = protocols,
+        .pvo = &pvo,
         .gid = -1,
         .uid = -1
     };
@@ -154,8 +306,8 @@ int main(void)
 
     if (context == NULL)
     {
-        fprintf(stderr, "libwebsocket init failed\n");
-        return -1;
+        lwsl_err("libwebsocket init failed\n");
+        return 1;
     }
 
     printf("starting server...\n");
@@ -165,12 +317,12 @@ int main(void)
     while (1)
     {
         // libwebsocket_service will process all waiting events with
-        // their callback functions and then wait for 50 miliseconds
+        // their callback functions and then wait for 10 miliseconds
         
         // this is a single threaded web server and this will keep our
         // server from generating load while there are no requests to process
 
-        lws_service(context, 50);
+        lws_service(context, 10);
     }
 
     printf("stopping server...");
